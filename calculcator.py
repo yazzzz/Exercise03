eae77c390e3bb2afaab4a0af04c73ca460f44d95
Hackbright Student <user@mini-002.(none)>
2014-02-05 15:52:36 -0800

    all functions working, checking length

--- calculcator.py
+++ calculcator.py
@@ -12,10 +12,30 @@ while True:
     user_input = raw_input(">> ")
     user_input_list = []
     user_input_list = user_input.split(" ")
-    if user_input_list[1].isdigit() and user_input_list[2].isdigit():
-        if user_input_list[0] == "+":
-            print arithmetic.add(int(user_input_list[1]), int(user_input_list[2]))
-    else:   
-        print "That's not a valid integer!"
+    arg1 = user_input_list[1]
+    arg2 = user_input_list[2]
+
+    if len(user_input_list) > 3:
+        print "too many numbers!"
+    else:
+        if arg1.isdigit() and arg2.isdigit():
+            if user_input_list[0] == "+":
+                print arithmetic.add(int(arg1), int(arg2))
+            if user_input_list[0] == "-":
+                print arithmetic.subtract(int(arg1), int(arg2))
+            if user_input_list[0] == "*":
+                print arithmetic.multiply(int(arg1), int(arg2))
+            if user_input_list[0] == "/":
+                print arithmetic.divide(int(arg1), int(arg2))
+            if user_input_list[0] == "square":
+                print arithmetic.square(int(arg1))
+            if user_input_list[0] == "cube":
+                print arithmetic.cube(int(arg1))
+            if user_input_list[0] == "pow":
+                print arithmetic.power(int(arg1), int(arg2))
+            if user_input_list[0] == "mod":
+                print arithmetic.mod(int(arg1), int(arg2))
+        else:   
+            print "That's not a valid integer!"
 
 
