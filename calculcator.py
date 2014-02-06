0c82748d7946d13da173acdb8186f0f826b0f59b
Hackbright Student <user@mini-002.(none)>
2014-02-05 16:28:38 -0800

    looks clunky, but passes all tests for input types and number of inputs

--- calculcator.py
+++ calculcator.py
@@ -12,12 +12,23 @@ while True:
     user_input = raw_input(">> ")
     user_input_list = []
     user_input_list = user_input.split(" ")
-    arg1 = user_input_list[1]
-    arg2 = user_input_list[2]
 
-    if len(user_input_list) > 3:
-        print "too many numbers!"
-    else:
+    if len(user_input_list) > 3 or len(user_input_list) < 2:
+        print "incorrect number of arguments! please try again!"
+    elif len(user_input_list) == 2:
+        arg1 = user_input_list[1]
+        if user_input_list[0] == "square" and arg1.isdigit():
+            print arithmetic.square(int(arg1))
+        elif user_input_list[0] == "cube" and arg1.isdigit():
+            print arithmetic.cube(int(arg1))
+        else:
+            print "That's not a valid integer!"
+
+
+    elif len(user_input_list) == 3:
+        arg1 = user_input_list[1]
+        arg2 = user_input_list[2]
+
         if arg1.isdigit() and arg2.isdigit():
             if user_input_list[0] == "+":
                 print arithmetic.add(int(arg1), int(arg2))
@@ -27,14 +38,11 @@ while True:
                 print arithmetic.multiply(int(arg1), int(arg2))
             if user_input_list[0] == "/":
                 print arithmetic.divide(int(arg1), int(arg2))
-            if user_input_list[0] == "square":
-                print arithmetic.square(int(arg1))
-            if user_input_list[0] == "cube":
-                print arithmetic.cube(int(arg1))
             if user_input_list[0] == "pow":
                 print arithmetic.power(int(arg1), int(arg2))
             if user_input_list[0] == "mod":
                 print arithmetic.mod(int(arg1), int(arg2))
+
         else:   
             print "That's not a valid integer!"
 
